36835124d1e332c4fac9c0b9cbef094e8e3bf3ab
Aashif Ahamed <aashifahdofficial@gmail.com>
2024-01-07 04:54:46 +0100

    fix: profile name changed

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,5 +1,37 @@
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const ProfilePosts = () => {
-  return <div>ProfilePosts</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      <Grid
+        templateColumns={{
+          sm: "repeat(1,1fr)",
+          md: "repeat(3,1fr)",
+        }}
+        gap={1}
+        columnGap={1}
+      >
+        {isLoading &&
+          [0, 1, 2, 3, 4, 5].map((_, idx) => (
+            <VStack key={idx} alignItems="flex-start" gap={4}>
+              <Skeleton w={"full"}>
+                <Box h="300px">Content Warapped</Box>
+              </Skeleton>
+            </VStack>
+          ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default ProfilePosts;
