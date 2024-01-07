8eef12162a3a4904cf416b5bcb02eb563c8610b7
Aashif Ahamed <aashifahdofficial@gmail.com>
2024-01-07 23:37:52 +0100

    Setup Signup With Email && Password

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getApp } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
